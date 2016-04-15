7bbc9ad696a42742e31a279c497c997aebdf916d
Mehael <mehael@mail.ru>
2016-04-15 23:47:03 +1000

    Added Spawn and DestroyOverTime Scripts

---
+++ Spawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    public GameObject[] possiblePrefabs;
+    public float spawnCooldown = 1f;
+    public float delayToStartSpawning = 1f;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", delayToStartSpawning, spawnCooldown);
+    }
+
+    void Spawn ()
+    {
+        int enemyIndex = Random.Range(0, possiblePrefabs.Length);
+        Instantiate(possiblePrefabs[enemyIndex], transform.position, Quaternion.identity);
+    }
+}
---
+++ destroyAfterTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class destroyAfterTime : MonoBehaviour {
+    public float timeToDestroy = 3f;
+
+	IEnumerator Start () {
+        yield return new WaitForSeconds(timeToDestroy);
+	    Destroy(gameObject);
+    }
+}
